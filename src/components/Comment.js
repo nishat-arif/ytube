059a60ac4cde37b4a680b269f66e5cdc323a21e5
nishat-arif <nishatarif1912@gmail.com>
2025-08-08 18:20:43 -0400

    comment section

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,37 @@
-const Comment = () => {
+import userIcon from '../assets/userIcon.png'
 
-    return (<div className="comments-container">
-        <h4>Comment..</h4>
+const Comment = ({data}) => {
+
+    const {name , text , replies} = data;
+
+    return (<div className="comment-container">
+                    <div className="comment-img">
+                            <img src={userIcon} className="user-icon" alt="user-logo" />     
+                    </div>
+                    <div className="comment">
+                        <p className="comment-name">{name}</p>
+                        <p className="comment-text">{text}</p>
+                    </div>
 
     </div>)
 
+}
+
+export const CommentList = ({commentdata}) =>{
+    return  (
+        <>
+
+            {commentdata.map((comment, idx) => (
+                    <div>
+                        <Comment key={idx} data={comment} />
+                        <div className="nested-comments"><CommentList commentdata={comment.replies}/></div>
+                    </div>        
+                ))}
+        
+        </>
+        
+        )
+        
 }
 
 export default Comment
--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,10 +1,58 @@
-import Comment from './Comment';
+import Comment , {CommentList}from './Comment';
 
 const CommentsContainer = () => {
+    
+    // n level deep comment
+    const CommentData = [
+        { name :"user1",
+          text:"this is comment text",
+          replies:[]
+        },
+         { name :"user2",
+          text:"this is comment text",
+          replies:[
+                    { name :"user3",
+                    text:"this is comment text",
+                    replies:[
+                         { name :"user2",
+                        text:"this is comment text",
+                        replies:[]
+                        },
+                         { name :"user1",
+                        text:"this is comment text",
+                        replies:[
+                             { name :"user3",
+                            text:"this is comment text",
+                            replies:[]
+                            }
+                        ]
+                        }
+                    ]
+                    }
+                    ]
+        },
+         { name :"user3",
+          text:"this is comment text",
+          replies:[
+            { name :"user2",
+                        text:"this is comment text",
+                        replies:[]
+                        }
+          ]
+        },
+         { name :"user1",
+          text:"this is comment text",
+          replies:[]
+        }, 
+        { name :"user5",
+          text:"this is comment text",
+          replies:[]
+        }
+    ]
 
     return (<div className="comments-container">
         <h4 className='comments-heading'>Comments</h4>
-        <div><Comment/></div>
+        <div><CommentList commentdata ={CommentData}/></div>
     </div>)
 
 }
